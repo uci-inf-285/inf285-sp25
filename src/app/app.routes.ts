ac640c5a6e1a7eeb8d546cf287c485f4d081b373
Daniel Epstein <epstein@ics.uci.edu>
2025-03-31 14:30:28 -0700

    Post A2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SyllabusComponent } from './syllabus/syllabus.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { A1Component } from './a1/a1.component';
+import { A2Component } from './a2/a2.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent},
@@ -13,4 +14,5 @@ export const routes: Routes = [
 	{ path: 'resources', component: ResourcesComponent},
 	{ path: 'calendar', component: CalendarComponent},
 	{ path: 'assignments/a1', component: A1Component},
+	{ path: 'assignments/a2', component: A2Component},
 ];
